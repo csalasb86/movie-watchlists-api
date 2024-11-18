bb8fb85b9046051d9d571ebc2d7bc2dc5ab071bd
Carlos Salas <carlos.salas@theoptimalpartner.com>
2024-11-18 12:37:30 -0300

    rubucop offenses

--- app/controllers/concerns/rack_sessions_fix.rb
+++ app/controllers/concerns/rack_sessions_fix.rb
@@ -1,11 +1,11 @@
-# Since we created a Rails API-only app, sessions are disabled by default, 
-# but devise relies on sessions to function. Before Rails 7, sessions were passed as a hash, 
-# so even if they were disabled, devise could still write into a session hash. 
-# Since Rails 7, a session is an ActionDispatch::Session object, 
-# which is not writable on Rails API-only apps, because the ActionDispatch::Session is disabled. 
+# Since we created a Rails API-only app, sessions are disabled by default,
+# but devise relies on sessions to function. Before Rails 7, sessions were passed as a hash,
+# so even if they were disabled, devise could still write into a session hash.
+# Since Rails 7, a session is an ActionDispatch::Session object,
+# which is not writable on Rails API-only apps, because the ActionDispatch::Session is disabled.
 # If we try to use devise in this configuration we will get the error:
 
-# ActionDispatch::Request::Session::DisabledSessionError 
+# ActionDispatch::Request::Session::DisabledSessionError
 # (Your application has sessions disabled. To write to the session you must first configure a session store):
 
 module RackSessionsFix
@@ -20,7 +20,7 @@ module RackSessionsFix
     before_action :set_fake_session
     private
     def set_fake_session
-      request.env['rack.session'] ||= FakeRackSession.new
+      request.env["rack.session"] ||= FakeRackSession.new
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
+  devise_for :users, path: "", path_names: {
+    sign_in: "login",
+    sign_out: "logout",
+    registration: "signup"
   },
   controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    sessions: "users/sessions",
+    registrations: "users/registrations"
   }
 end
